4b0357b2331b4025f254ea147620cfb260dd30c3
bakinovsky-m <makbak@ya.ru>
2017-12-13 02:47:55 +0300

    grad: cleaning

--- grad.py
+++ grad.py
@@ -4,11 +4,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def grad_x(x, y, z):
-	# return 2*(10*(x**3) + 3*(x**2)*(y+2) - 2*x*(3*(y**2) - 3*y+7) + (y**3) - 8*y +2)
 	return 2 * (0.1 - pow(x, 2) + 2 * y*z - x)*(-2 * x - 1) - 6 * (-0.2 + pow(y, 2) - 3 * x*z - y)*z - 4 * (0.3 - pow(z, 2) - 2 * x*y - z)*y
 
 def grad_y(x, y, z):
-	# return 2*((x**3) +(x**2)*(3-6*y) + x*(3*(y**2)-8) + 10*(y**3) - 3*y + 16*y - 16)
 	return 4 * (0.1 - pow(x, 2) + 2 * y*z - x)*z + 2 * (-0.2 + pow(y, 2) - 3 * x*z - y)*(2 * y - 1) - 4 * (0.3 - pow(z, 2) - 2 * x*y - z)*x
 
 def grad_z(x, y, z):
@@ -21,7 +19,6 @@ yk_old = 0
 zk = 2
 zk_old = 0
 
-# lambd = [e^-4; e^-9]
 lambd = math.exp(-5)
 sigm = 0.1
 eps = 1e-4
@@ -40,15 +37,13 @@ xk_arr = []
 yk_arr = []
 zk_arr = []
 
-while max(abs(xk - xk_old), abs(yk - yk_old), abs(zk - zk_old)) > eps and count < 5000:
+while max(abs(xk - xk_old), abs(yk - yk_old), abs(zk - zk_old)) > eps:
 	count += 1
 	
 	xk_old = xk
 	yk_old = yk
 	zk_old = zk
 
-	# xk = xk_old - lambd * grad_x(xk_old, yk_old)
-	# yk = yk_old - lambd * grad_y(xk_old, yk_old)
 	xk = xk_old - lambd * grad_x(xk_old, yk_old, zk_old)
 	yk = yk_old - lambd * grad_y(xk_old, yk_old, zk_old)
 	zk = zk_old - lambd * grad_z(xk_old, yk_old, zk_old)
